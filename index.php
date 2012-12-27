a59cef885954360256dd9986695acad89d5020f9
digitalnature <hello@digitalnature.eu>
2012-12-27 21:18:18 +0200

    Added some more css rules

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ require __DIR__ . '/function.highlight.php';
 $code = file_get_contents(__FILE__);
 $code = highlight($code);
 
+// not really required :)
 header('Content-type: text/html');
 
 ?>
@@ -12,7 +13,7 @@ header('Content-type: text/html');
 <html>
   <body>
 
-  	<?php print $code; ?>
+    <?php print $code; ?>
 
   </body>
 <html>
